67f9564d41dc187868c6b27845a7088ea3fe16a3
mamadkiller2 <mamadkillergg@gmail.com>
2017-03-10 12:33:49 +0330

    Update setwlc.lua

--- plugins/setwlc.lua
+++ plugins/setwlc.lua
@@ -1,60 +1,58 @@
-do
 local function run(msg, matches, callback, extra)
+
 local data = load_data(_config.moderation.data)
-local rules = data[tostring(msg.to.id)]['rules']
-local about = data[tostring(msg.to.id)]['description']
-local hash = 'group:'..msg.to.id
-local group_welcome = redis:hget(hash,'welcome')
+
+local group_welcome = data[tostring(msg.to.id)]['group_welcome']
+-------------------------- Data Will be save on Moderetion.json
+    
 if matches[1] == 'delwlc' and not matches[2] and is_owner(msg) then 
     
-   redis:hdel(hash,'welcome')
-        return 'متن خوش آمد گویی پاک شد 🗑'
+   data[tostring(msg.to.id)]['group_welcome'] = nil --delete welcome
+        save_data(_config.moderation.data, data)
+        
+        return '💫خوش آمدگویی گروه حذف شد💫'
+end
+if not is_owner(msg) then 
+    return '💫تنها برای مالکان گروه💫'
+end
+--------------------Loading Group Rules
+local rules = data[tostring(msg.to.id)]['rules']
+    
+if matches[1] == 'rules' and matches[2] and is_owner(msg) then
+    if data[tostring(msg.to.id)]['rules'] == nil then --when no rules found....
+        return '💫هیچ قانونی یافت نشد!\n\nابتدا قوانین رابا /setrules [متن قوانین]\nیا \nبااستفاده از خوش آمدگویی پیشفرض  /setwlc [متن خوش آمدگویی] ثبت کنید💫'
+end
+data[tostring(msg.to.id)]['group_welcome'] = matches[2]..'\n\n💫قوانین گروه💫 :\n'..rules
+        save_data(_config.moderation.data, data)
+        
+        return '💫خوش آمدگویی گروه ثبت شد💫 :\n'..matches[2]
+end
+if not is_owner(msg) then 
+    return '💫تنها برای مالکان💫!'
 end
 
-local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "No connection" end
-local jdat = json:decode(url)
-
-if matches[1] == 'setwlc' and is_owner(msg) then
-redis:hset(hash,'welcome',matches[2])
-        return 'متن خوش آمد گویی گروه تنظیم شد به : \n'..matches[2]
+if matches[1] and is_owner(msg) then
+    
+data[tostring(msg.to.id)]['group_welcome'] = matches[1]
+        save_data(_config.moderation.data, data)
+        
+        return '💫خوش آمدگویی گروه ثبت شد💫 : \n'..matches[1]
+end
+if not is_owner(msg) then 
+    return '💫تنها برای مالکان💫!'
 end
 
-if matches[1] == 'chat_add_user' or 'chat_add_user_link' or 'channel_invite' and msg.service then
-group_welcome = string.gsub(group_welcome, '{gpname}', msg.to.title)
-group_welcome = string.gsub(group_welcome, '{firstname}', ""..(msg.action.user.first_name or '').."")
- group_welcome = string.gsub(group_welcome, '{lastname}', ""..(msg.action.user.last_name or '').."")
-  group_welcome = string.gsub(group_welcome, '{username}', "@"..(msg.action.user.username or '').."")
-  group_welcome = string.gsub(group_welcome, '{fatime}', ""..(jdat.FAtime).."")
-  group_welcome = string.gsub(group_welcome, '{entime}', ""..(jdat.ENtime).."")
-  group_welcome = string.gsub(group_welcome, '{fadate}', ""..(jdat.FAdate).."")
-  group_welcome = string.gsub(group_welcome, '{endate}', ""..(jdat.ENdate).."")
-  group_welcome = string.gsub(group_welcome, '{rules}', ""..(rules or '').."")
-  group_welcome = string.gsub(group_welcome, '{about}', ""..(about or '').."")
 
-
-
-
-group_welcome = string.gsub(group_welcome, '{نام گروه}', msg.to.title)
-group_welcome = string.gsub(group_welcome, '{نام اول}', ""..(msg.action.user.first_name or '').."")
- group_welcome = string.gsub(group_welcome, '{نام آخر}', ""..(msg.action.user.last_name or '').."")
-  group_welcome = string.gsub(group_welcome, '{نام کاربری}', "@"..(msg.action.user.username or '').."")
-  group_welcome = string.gsub(group_welcome, '{ساعت فارسی}', ""..(jdat.FAtime).."")
-  group_welcome = string.gsub(group_welcome, '{ساعت انگلیسی}', ""..(jdat.ENtime).."")
-  group_welcome = string.gsub(group_welcome, '{تاریخ فارسی}', ""..(jdat.FAdate).."")
-  group_welcome = string.gsub(group_welcome, '{تاریخ انگلیسی}', ""..(jdat.ENdate).."")
-
- end
-return group_welcome
+    
 end
 return {
   patterns = {
-  "^[!#/](setwlc) +(.*)$",
-  "^[!#/](delwlc)$",
-  "^!!tgservice (chat_add_user)$",
-  "^!!tgservice (channel_invite)$",
-  "^!!tgservice (chat_add_user_link)$",
+  "^[!/]setwlc (rules) +(.*)$",
+  "^[!/]setwlc +(.*)$",
+  "^[!/](delwlc)$",
+  "^setwlc (rules) +(.*)$",
+  "^setwlc +(.*)$",
+  "^(delwlc)$"
   },
   run = run
 }
-end
