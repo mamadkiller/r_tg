ad7f20b39d1482e4d27fd619828f825d3de8a41b
mamadkiller2 <mamadkillergg@gmail.com>
2017-03-10 13:20:47 +0330

    Update font.lua

--- plugins/font.lua
+++ plugins/font.lua
@@ -77,7 +77,7 @@ local function run(msg, matches)
 		a=a+1
 		result_text = result_text..a.."- "..result[a].."\n\n"
 	end
-	return result_text.."______________________________\n@Punisher_TG"
+	return result_text.."______________________________\n@mamadiller"
 end
 
 return {
